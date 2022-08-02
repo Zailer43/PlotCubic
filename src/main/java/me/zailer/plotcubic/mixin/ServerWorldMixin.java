f0eff95d8b4b574a06a82fa7984958fd8d05e5dc
Zailer43 <minecraftpelusa@gmail.com>
2022-08-02 17:13:39 -0300

    Don't send rain packets to all worlds * Fix https://github.com/Zailer43/PlotCubic/issues/3

--- src/main/java/me/zailer/plotcubic/mixin/ServerWorldMixin.java
+++ src/main/java/me/zailer/plotcubic/mixin/ServerWorldMixin.java
@@ -1,18 +1,21 @@
 package me.zailer.plotcubic.mixin;
 
-import me.zailer.plotcubic.plot.Plot;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.Packet;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.PlayerManager;
+import net.minecraft.server.world.ServerChunkManager;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ServerWorld.class)
 public abstract class ServerWorldMixin {
+
+    @Shadow public abstract ServerChunkManager getChunkManager();
 
     @Redirect(
             method = "canPlayerModifyAt",
@@ -23,5 +26,20 @@ public abstract class ServerWorldMixin {
     )
     private boolean isSpawnProtected(MinecraftServer instance, ServerWorld world, BlockPos pos, PlayerEntity player) {
         return false;
+    }
+
+    @Redirect(
+            method = "tickWeather",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/server/PlayerManager;sendToAll(Lnet/minecraft/network/Packet;)V"
+            )
+
+    )
+    private void dontSendRainPacketsToAllWorlds(PlayerManager instance, Packet<?> packet) {
+        // Vanilla sends rain packets to all players when rain starts in a world,
+        // even if they are not in it, meaning that if it is possible to rain in the world they are in,
+        // the rain effect will remain until the player changes dimension or reconnects.
+        instance.sendToDimension(packet, this.getChunkManager().getWorld().getRegistryKey());
     }
 }
